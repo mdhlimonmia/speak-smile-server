08cbffa25f9584be08089f73b957442e4ea457e8
Mdhlimon <mdhlimonmia@gmail.com>
2023-06-12 15:36:02 +0600

    Update status done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -79,6 +79,16 @@ async function run() {
       res.send(result);
     });
    
+    app.patch("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const data= req.body
+      const updateUser = {$set: data };
+      // console.log(query, data);
+      const result = await usersCollection.updateOne(query, updateUser)
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
